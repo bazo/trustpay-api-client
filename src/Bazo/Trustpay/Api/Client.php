ee2b6d87221022bdd6513e9b0d0d7f2a02f1db99
Martin Bazik <martin@bazo.sk>
2014-03-07 14:03:28 +0100

    exactly two decimal places

--- src/Bazo/Trustpay/Api/Client.php
+++ src/Bazo/Trustpay/Api/Client.php
@@ -63,7 +63,7 @@ class Client
 
 		$query = array(
 			'AID' => $this->aid,
-			'AMT' => $amount,
+			'AMT' => round($amount, 2),
 			'CUR' => $currency,
 			'REF' => $ref,
 			'SIG' => $this->signRequest($message),
